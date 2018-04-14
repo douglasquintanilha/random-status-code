d2d40f12de15426c10e618081b9a2cdba16ff918
Douglas Quintanilha <dougquintanilha@gmail.com>
2018-04-14 11:24:23 -0300

    Removed blank lines

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 
 const port = process.env.PORT || 3000;
-
-
 const statusCodes = [ 200, 201, 202, 203, 400, 401, 403, 404, 500, 501];
 
 app.get('/', function (req, res) {
@@ -17,7 +15,6 @@ app.get('/', function (req, res) {
         res.status(200).send('This was a success! Status code: 200');
     }
 });
-
 
 app.listen(port, function () {
     console.log('Server initialiazed on port ' + port);
