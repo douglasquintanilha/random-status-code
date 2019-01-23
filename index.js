c376ad7f66e385ba9506afdb3c9e9611881ba62d
Douglas Quintanilha <dougquintanilha@gmail.com>
2019-01-23 18:24:02 -0200

    Added 418 status code.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 const port = process.env.PORT || 3000;
-const statusCodes = [ 200, 201, 202, 203, 400, 401, 403, 404, 500, 501];
+const statusCodes = [ 200, 201, 202, 203, 400, 401, 403, 404, 418, 500, 501];
 
 app.get('/', function (req, res) {
     let smallRandom = Math.random();
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
         let randomInt = parseInt(smallRandom * badStatusCodes.length);
         res.status(badStatusCodes[randomInt]).send('The response status code was: '+ badStatusCodes[randomInt]);
     }else{
-        res.status(200).send('This was a success? Status code: 200');
+        res.status(200).send('This was a success! Status code: 200');
     }
 });
 
